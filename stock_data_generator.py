32f089127a97ee56af1647187be76d3a8eed7bab
CSC <chancsc@gmail.com>
2023-07-19 18:42:00 +0800

    Update stock_data_generator.py

--- stock_data_generator.py
+++ stock_data_generator.py
@@ -6,7 +6,7 @@
 import os
 import yfinance as yf
 import pandas as pd
-from datetime import date
+from datetime import date, datetime
 
 def generate_stock_data(symbol, start_date=None, end_date=None):
     if start_date is None:
@@ -17,15 +17,6 @@ def generate_stock_data(symbol, start_date=None, end_date=None):
     start_date = pd.to_datetime(start_date, dayfirst=True).strftime('%Y-%m-%d')
     end_date = pd.to_datetime(end_date, dayfirst=True).strftime('%Y-%m-%d')
 
-    # Download stock data
-    data = yf.download(symbol, start=start_date, end=end_date)
-
-    # Select required columns
-    data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
-
-    # Reset index
-    data.reset_index(inplace=True)
-
     # Define the sub-directory path
     sub_directory = 'backtesting/test/'
 
@@ -33,15 +24,28 @@ def generate_stock_data(symbol, start_date=None, end_date=None):
     if not os.path.exists(sub_directory):
         os.makedirs(sub_directory)
 
-    # Delete existing CSV file if it exists
+    # Delete existing CSV file if it exists and its date is older than today
     csv_file = os.path.join(sub_directory, f'{symbol}.csv')
     if os.path.exists(csv_file):
-        os.remove(csv_file)
-        print('Existing CSV file deleted.')
+        file_modified_date = datetime.fromtimestamp(os.path.getmtime(csv_file)).date()
+        today = date.today()
+        if file_modified_date < today:
+            os.remove(csv_file)
+            print('Existing CSV file deleted.')
 
-    # Save data to CSV
-    data.to_csv(csv_file, index=False)
+            # Download stock data
+            data = yf.download(symbol, start=start_date, end=end_date)
+            # Select required columns
+            data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
+            # Reset index
+            data.reset_index(inplace=True)
+            # Save data to CSV
+            data.to_csv(csv_file, index=False)
 
-    print('Data downloaded successfully.')
+            print('Data downloaded successfully.')
+
+
+# Generate stock data with default parameters
+#generate_stock_data('TSLA')
 
 
