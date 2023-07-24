bb2886d7baa1547970a3f350722c48bfc2be26c6
CSC <chancsc@gmail.com>
2023-07-24 10:47:52 +0800

    Update check_bss.py

--- check_bss.py
+++ check_bss.py
@@ -1,11 +1,11 @@
-# This python script is used to Check Buy/Sell Signal (check_bss) & chart
-# it can display the chart, if there's buy/sell signal, send telegram notification
-
-# e.g. usage: 
-# 1. python check_bss.py MSFT
-# 2. python check_bss.py MSFT --oBrowser=1 (optional parameter:
-#    1 = open browser and display chart
-#    0 = default value if parameter omitted, will not open browser )
+# This  script is used to Check Buy/Sell Signal (check_bss) & chart.
+# It can display the chart, if there's buy/sell signal, send telegram notification
+#
+# it will retrieve current year data by default, use get_stock_data_SA.py to get 
+# data for different period
+#
+# browser & table parameter are optional, if ommitted, will display table in 
+# console, and open browser to display chart
 
 import sys
 from backtesting import Backtest, Strategy
@@ -15,18 +15,34 @@ from backtesting.test.__init__ import SMA
 
 from get_stock_data import generate_stock_data
 import argparse
+import datetime
+
+current_year = datetime.date.today().year
 
 parser = argparse.ArgumentParser(description='Display stock chart in a web browser.')
-parser.add_argument('stockSymbol', type=str, help='The stock symbol to analyse')
-parser.add_argument('--oBrowser', type=int, choices=[0, 1],
-                    help='Optional parameter for displaying browser (0 for default, 1 for alternative)')
+parser.add_argument('stockSymbol', type=str, help='The stock symbol to analyze')
+parser.add_argument('--browser', type=int, choices=[0, 1], default=1,
+                    help='Optional parameter for displaying browser (1 = open browser)')
+parser.add_argument('--table', type=int, choices=[0, 1], default=1,
+                    help='Optional parameter for hiding table in console (1 = Display)')
+parser.add_argument('--year', type=int, default=datetime.date.today().year,
+                    help='Optional parameter for specifying the year')
 
 args = parser.parse_args()
 stockSymbol = args.stockSymbol
-oBrowser = args.oBrowser
+Browser = args.browser
+sTable = args.table
+
+if args.year == current_year:
+    sdate = None
+    edate = None
+else:
+    year = args.year
+    sdate = f"01/01/{args.year}"
+    edate = f"31/12/{args.year}"
 
 # Example usage:
-generate_stock_data(stockSymbol)
+generate_stock_data(stockSymbol, sdate, edate)
 
 # Use the stock symbol to load the corresponding stock data
 stockTicker = _read_file(stockSymbol)  # Replace `_read_file` with the actual function to load stock data
@@ -34,8 +50,8 @@ stockTicker = _read_file(stockSymbol)  # Replace `_read_file` with the actual fu
 class SmaCross(Strategy):
     def init(self):
         price = self.data.Close
-        self.ma1 = self.I(SMA, price, 10)
-        self.ma2 = self.I(SMA, price, 20)
+        self.ma1 = self.I(SMA, price, 5)
+        self.ma2 = self.I(SMA, price, 10)
 
     def next(self):
         if crossover(self.ma1, self.ma2):
@@ -47,4 +63,4 @@ class SmaCross(Strategy):
 
 bt = Backtest(stockTicker, SmaCross, commission=.002, exclusive_orders=True)
 stats = bt.run()
-bt.plot(filename=stockSymbol, open_browser=oBrowser)
+bt.plot(filename=stockSymbol, open_browser=Browser, showTable=sTable)
