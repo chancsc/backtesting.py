55d27972672cb8e7d662cedd50818b3926a9eb3c
CSC <chancsc@gmail.com>
2023-07-24 10:47:17 +0800

    Update check_bss_batch.py

--- check_bss_batch.py
+++ check_bss_batch.py
@@ -12,19 +12,40 @@ Instructions:
    Example: python check_bss.py TSLA
 3. The output of 'check_bss.py' will be displayed in the terminal for each stock ticker.
 
-Sample data of 'stock_list.txt':
+Sample data of 'stock_list_us.json':
 AAPL
 ABNB
 AMZN
 
+Usage:
+
+Parameter
+file: optional, if omitted, default to stock_list_us.json
+year: optional, if omitted, default to current year
+
+Note: 
+if runnign the batch multiple time a day, please manually remove the csv file to force refresh / redownload
+
+$ ptyon check_bss_batch.py
+$ ptyon check_bss_batch.py --file=stock_list_asia.json
+$ ptyon check_bss_batch.py --file=stock_list_test.json --year=2022
+$ ptyon check_bss_batch.py --year=2022 > PL.txt 2>&1   (<-- output the result to PL.txt)
 """
 
 import subprocess
 import json
 import argparse
+import datetime
 
-def execute_check_bss(stock_ticker):
-    cmd = f"python check_bss.py {stock_ticker}"
+def execute_check_bss(stock_ticker, year=None):
+    # batch mode, don't need to display the BSS table in console, and don't open browser
+    cmd = f"python check_bss.py {stock_ticker} --browser=0 --table=0"
+
+    if year:
+        # Modify the command to include the year value
+        cmd += f" --year={year}"
+        
+    # print("cmd = ", {cmd})
     try:
         subprocess.run(cmd, shell=True, check=True)
     except subprocess.CalledProcessError as e:
@@ -32,9 +53,11 @@ def execute_check_bss(stock_ticker):
         print(e)
 
 def main():
-    parser = argparse.ArgumentParser(description='Check BSS for stock tickers.')
+    parser = argparse.ArgumentParser(description='Check BSS for stocks.')
     parser.add_argument('--file', type=str, default='stock_list_us.json',
                         help='Path to JSON file containing stock data (default: stock_list_us.json)')
+    parser.add_argument('--year', type=int, default=datetime.date.today().year,
+                        help='Year for checking BSS (optional)')
     args = parser.parse_args()
 
     with open(args.file, "r") as file:
@@ -42,9 +65,11 @@ def main():
 
     for stock in stock_data:
         ticker = stock["ticker"]
-        name = stock["name"]
-        print(f"Checking for: {name}")
-        execute_check_bss(ticker)
+        execute_check_bss(ticker, args.year)
+        # name = stock["name"]
+        # print("")
+        # print(f"Checking for: {name}")
+
 
 if __name__ == "__main__":
     main()
